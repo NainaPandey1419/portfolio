5994b8fa474979f9222faf7983bcd2f89d6e414e
NainaPandey1419 <naina.1206.pandey@gmail.com>
2024-09-14 21:24:19 +0530

    fixed exp

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,7 @@
 import hero from './hero.jpg'
 import gfg from './gfg.jpeg'
 import startup_cell_mits from './startup_cell_mits.jpeg'
+import dashtiny from './dashtiny.jpeg'
 import logo from './logo.svg'
 
 export {
